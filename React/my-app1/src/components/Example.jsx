2b7a224975b74743e88b0ef5e0b4d9724ef5dbbe
Emr25 <129274828+Emr25@users.noreply.github.com>
2024-01-21 21:55:47 +0300

    React
    React Örnek 1

---
+++ React/my-app1/src/components/Example.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const Example = () => {
+
+    const[counter,setCounter] = useState(0)
+
+
+
+  return (
+    <div>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <span>{counter}</span>
+        <button onClick={()=> setCounter(counter -1)}>-</button>
+    </div>
+  )
+}
+
+export default Example
